ab42e421ff3de110e2081ca4a8e37987c4627788
Mehedi6023 <quazimehedihasanovi@gmail.com>
2025-06-05 13:50:47 +0600

    initialized files

---
+++ src/router/routes.jsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home";
+import Root from "../layouts/Root";
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    Component: Root,
+    children: [
+      {
+        index: true,
+        Component: Home
+      }
+    ]
+  }
+])
